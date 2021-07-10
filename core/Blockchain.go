38ebc4aee81d534ccaf2f8bfe50048af1e9f2a23
JinkwanLee <leejinkwan@kunsan.ac.kr>
2021-07-10 09:14:14 +0900

    first commit

---
+++ core/Blockchain.go
@@ -0,0 +1,25 @@
+
+package core
+
+import (
+	"github.com/JinkwanLee/go-blockchain/core"
+)
+// 
+type Blockchain struct {
+        blocks []*Block
+}
+
+
+func (bc *Blockchain) AddBlock(data string) {
+        prevBlock := bc.blocks[len(bc.blocks)-1]
+        newBlock := NewBlock(data, prevBlock.Hash)
+        bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+        return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+        return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JinkwanLee/go-blockchain/v1.0.0
+
+go 1.16
+
+require github.com/JinkwanLee/go-blockchain v0.0.0-20210709215812-fae91a0a3873
